3bc1478d8eeb5787f82c6262531649a61b0db4f9
SindreKvam <s.m.k.1@hotmail.com>
2020-10-30 10:34:09 +0100

    Functioning binary to bcd converter

---
+++ binary_to_bcd.vhd
@@ -0,0 +1,48 @@
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+entity binary_to_bcd is
+  port(
+  binary : in std_logic_vector(7 downto 0);
+
+  bcd_0 : out std_logic_vector(3 downto 0) := (others => '0'); -- One
+  bcd_1 : out std_logic_vector(3 downto 0) := (others => '0') -- Ten
+  );
+end entity;
+
+
+architecture rtl of binary_to_bcd is
+begin
+
+process(binary) is
+  variable a : std_logic_vector(3 downto 0);
+  variable b : std_logic_vector(3 downto 0);
+  begin
+    a := (others => '0');
+    b := (others => '0');
+
+    for i in 7 downto 0 loop
+
+      if unsigned(a) > 4 then
+        a := std_logic_vector(unsigned(a) + 3);
+
+      end if;
+      if unsigned(b) > 4 then
+        b := std_logic_vector(unsigned(b) + 3);
+
+      end if;
+
+      b := b sll 1; --sll - shift logic left
+      b(0) := a(3);
+
+      a := a sll 1;
+      a(0) := binary(i);
+    end loop;
+
+    bcd_0 <= a;
+    bcd_1 <= b;
+
+  end process;
+end architecture;
---
+++ binary_to_bcd_tb.vhd
@@ -0,0 +1,32 @@
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+entity binary_to_bcd_tb is
+end entity;
+
+
+architecture rtl of binary_to_bcd_tb is
+  signal binary_tb : std_logic_vector(7 downto 0) := (others => '0');
+
+  signal bcd_0_tb : std_logic_vector(3 downto 0); -- One
+  signal bcd_1_tb : std_logic_vector(3 downto 0); -- Ten
+begin
+
+  dut : entity work.binary_to_bcd
+  port map(
+    binary => binary_tb,
+
+    bcd_0 => bcd_0_tb,
+    bcd_1 => bcd_1_tb
+  );
+
+  process is
+  begin
+
+    binary_tb <= X"15";
+    wait for 10 ns;
+
+  end process;
+end architecture;
