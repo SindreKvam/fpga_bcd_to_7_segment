2f175b52450a4b2f9d954e66110a141efb9d5c77
SindreKvam <s.m.k.1@hotmail.com>
2020-10-30 11:04:16 +0100

    Improved the bcd to 7 segment converter

--- bcd_to_7_segment.vhd
+++ bcd_to_7_segment.vhd
@@ -1,14 +1,16 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 
 entity bcd_to_7_segment is
 port(
-	D0 : in std_logic;
-	D1 : in std_logic;
-	D2 : in std_logic;
-	D3 : in std_logic;
+--	D0 : in std_logic;
+--	D1 : in std_logic;
+--	D2 : in std_logic;
+--	D3 : in std_logic;
+	digit : in std_logic_vector(3 downto 0);
 
 	a : out std_logic;
 	b : out std_logic;
@@ -24,17 +26,25 @@ end entity;
 architecture rtl of bcd_to_7_segment is
 begin
 
-	process(D0, D1, D2, D3) is
+	process(digit) is
 	begin
 
 			-- The not on every output is because the 7-segment display is using common anode instead of cathode
-		a <= not (D0 or D2 or (D1 and D3) or (not D1 and not D3));
-		b <= not (not D1 or (not D2 and not D3) or (D2 and D3));
-		c <= not (D1 or not D2 or D3);
-		d <= not ((D2 and not D1) or (not D1 and not D3) or (D2 and not D3) or D0 or (D1 and not D2 and D3));
-		e <= not ((not D1 and not D3) or (D2 and not D3));
-		f <= not (D0 or (not D2 and not D3) or (D1 and not D2) or (D1 and not D3));
-		g <= not (D0 or (D1 and not D2) or (not D1 and D2) or (D2 and not D3));
+--		a <= not (D0 or D2 or (D1 and D3) or (not D1 and not D3));
+--		b <= not (not D1 or (not D2 and not D3) or (D2 and D3));
+--		c <= not (D1 or not D2 or D3);
+--		d <= not ((D2 and not D1) or (not D1 and not D3) or (D2 and not D3) or D0 or (D1 and not D2 and D3));
+--		e <= not ((not D1 and not D3) or (D2 and not D3));
+--		f <= not (D0 or (not D2 and not D3) or (D1 and not D2) or (D1 and not D3));
+--		g <= not (D0 or (D1 and not D2) or (not D1 and D2) or (D2 and not D3));
+
+		a <= not (digit(0) or digit(2) or (digit(1) and digit(3)) or (not digit(1) and not digit(3)));
+		b <= not (not digit(1) or (not digit(2) and not digit(3)) or (digit(2) and digit(3)));
+		c <= not (digit(1) or not digit(2) or digit(3));
+		d <= not ((digit(2) and not digit(1)) or (not digit(1) and not digit(3)) or (digit(2) and not digit(3)) or digit(0) or (digit(1) and not digit(2) and digit(3)));
+		e <= not ((not digit(1) and not digit(3)) or (digit(2) and not digit(3)));
+		f <= not (digit(0) or (not digit(2) and not digit(3)) or (digit(1) and not digit(2)) or (digit(1) and not digit(3)));
+		g <= not (digit(0) or (digit(1) and not digit(2)) or (not digit(1) and digit(2)) or (digit(2) and not digit(3)));
 
 	end process;
 
--- bcd_to_7_segment_tb.vhd
+++ bcd_to_7_segment_tb.vhd
@@ -1,6 +1,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 
 entity bcd_to_7_segment_tb is
@@ -8,10 +9,11 @@ end entity;
 
 
 architecture tb of bcd_to_7_segment_tb is
-	signal D0_tb : std_logic;
-	signal D1_tb : std_logic;
-	signal D2_tb : std_logic;
-	signal D3_tb : std_logic;
+--	signal D0_tb : std_logic;
+--	signal D1_tb : std_logic;
+--	signal D2_tb : std_logic;
+--	signal D3_tb : std_logic;
+	signal digit_tb : std_logic_vector(3 downto 0) := (others => '0');
 
 	signal a_tb : std_logic;
 	signal b_tb : std_logic;
@@ -23,10 +25,12 @@ architecture tb of bcd_to_7_segment_tb is
 begin
 	dut : entity work.bcd_to_7_segment
 	port map(
-	D0	=> D0_tb,
-	D1	=> D1_tb,
-	D2	=> D2_tb,
-	D3	=> D3_tb,
+--	D0	=> D0_tb,
+--	D1	=> D1_tb,
+--	D2	=> D2_tb,
+--	D3	=> D3_tb,
+	digit => digit_tb,
+
 	a 	=> a_tb,
 	b 	=> b_tb,
 	c 	=> c_tb,
@@ -37,65 +41,94 @@ begin
 	);
 	process is
 	begin
-	D0_tb <= '0';
-	D1_tb <= '0';
-	D2_tb <= '0';
-	D3_tb <= '0';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(0, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '0';
-	D2_tb <= '0';
-	D3_tb <= '1';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(1, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '0';
-	D2_tb <= '1';
-	D3_tb <= '0';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(2, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '0';
-	D2_tb <= '1';
-	D3_tb <= '1';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(3, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '1';
-	D2_tb <= '0';
-	D3_tb <= '0';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(4, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '1';
-	D2_tb <= '0';
-	D3_tb <= '1';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(5, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '1';
-	D2_tb <= '1';
-	D3_tb <= '0';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(6, 4));
+		wait for 1 ns;
 
-	D0_tb <= '0';
-	D1_tb <= '1';
-	D2_tb <= '1';
-	D3_tb <= '1';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(7, 4));
+		wait for 1 ns;
 
-	D0_tb <= '1';
-	D1_tb <= '0';
-	D2_tb <= '0';
-	D3_tb <= '0';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(8, 4));
+		wait for 1 ns;
 
-	D0_tb <= '1';
-	D1_tb <= '0';
-	D2_tb <= '0';
-	D3_tb <= '1';
-	wait for 1 ns;
+		digit_tb <= std_logic_vector(to_unsigned(9, 4));
+		wait for 1 ns;
+--	D0_tb <= '0';
+--	D1_tb <= '0';
+--	D2_tb <= '0';
+--	D3_tb <= '0';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '0';
+--	D2_tb <= '0';
+--	D3_tb <= '1';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '0';
+--	D2_tb <= '1';
+--	D3_tb <= '0';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '0';
+--	D2_tb <= '1';
+--	D3_tb <= '1';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '1';
+--	D2_tb <= '0';
+--	D3_tb <= '0';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '1';
+--	D2_tb <= '0';
+--	D3_tb <= '1';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '1';
+--	D2_tb <= '1';
+--	D3_tb <= '0';
+--	wait for 1 ns;
+--
+--	D0_tb <= '0';
+--	D1_tb <= '1';
+--	D2_tb <= '1';
+--	D3_tb <= '1';
+--	wait for 1 ns;
+--
+--	D0_tb <= '1';
+--	D1_tb <= '0';
+--	D2_tb <= '0';
+--	D3_tb <= '0';
+--	wait for 1 ns;
+--
+--	D0_tb <= '1';
+--	D1_tb <= '0';
+--	D2_tb <= '0';
+--	D3_tb <= '1';
+--	wait for 1 ns;
 
 	end process;
 end architecture;
