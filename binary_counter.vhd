06e29421faaaf8a86a9fe4d4b749b34f0c188003
SindreKvam <s.m.k.1@hotmail.com>
2020-10-30 13:01:53 +0100

    binary counter now counts seconds, minutes and hours

--- binary_counter.vhd
+++ binary_counter.vhd
@@ -10,7 +10,9 @@ port(
 	rst_n : in std_logic;
 	cnt : in std_logic_vector(31 downto 0); -- Just needs to hold 50 million because we're operating at 50 MHz
 
-	binary_o : out std_logic_vector(7 downto 0) := (others => '0')
+	binary_h : out std_logic_vector(7 downto 0) := (others => '0');
+	binary_m : out std_logic_vector(7 downto 0) := (others => '0');
+	binary_s : out std_logic_vector(7 downto 0) := (others => '0')
 	);
 
 end entity;
@@ -32,12 +34,27 @@ begin
 				else
 					current_count <= 0;
 
-					binary_o <= std_logic_vector(unsigned(binary_o) + 1);
+					if unsigned(binary_s) >= 59 then
+						binary_s <= (others => '0');
 
-					if unsigned(binary_o) >= 99 then
-						binary_o <= (others => '0');
+						if unsigned(binary_m) >= 59 then
+							binary_m <= (others => '0');
+
+							if unsigned(binary_h) >= 23 then
+								binary_h <= (others => '0');
+
+							else
+								binary_h <= std_logic_vector(unsigned(binary_h) +1);
+							end if;
+
+						else
+							binary_m <= std_logic_vector(unsigned(binary_m) +1);
+						end if;
+
+					else
+						binary_s <= std_logic_vector(unsigned(binary_s) + 1);
+
 					end if;
-
 
 				end if;
 			end if;
--- binary_counter_tb.vhd
+++ binary_counter_tb.vhd
@@ -12,13 +12,17 @@ architecture tb of counter_tb is
 	signal rst_n_tb : std_logic := '0';
 	signal cnt_tb 	: std_logic_vector(31 downto 0) := (others => '0');
 
-	signal binary_o_tb : std_logic_vector(7 downto 0);
+	signal binary_h_tb : std_logic_vector(7 downto 0);
+	signal binary_m_tb : std_logic_vector(7 downto 0);
+	signal binary_s_tb : std_logic_vector(7 downto 0);
 
 begin
 	dut : entity work.counter
 	port map(
 
-	binary_o => binary_o_tb,
+	binary_h => binary_h_tb,
+	binary_m => binary_m_tb,
+	binary_s => binary_s_tb,
 
 	clk	=> clk_tb,
 	rst_n	=> rst_n_tb,
@@ -32,7 +36,7 @@ begin
 
 		rst_n_tb <= '1' after 13 ns;
 
-		cnt_tb <= X"00000F00";
+		cnt_tb <= X"000000F0";
 
 	end process;
 end architecture;
