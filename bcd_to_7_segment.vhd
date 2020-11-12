8edfcd4013d18e77d5fe2cb69bfdd9669d4f732c
SindreKvam <s.m.k.1@hotmail.com>
2020-11-12 11:09:53 +0100

    added dots to separate hours.minutes.seconds.

--- bcd_to_7_segment.vhd
+++ bcd_to_7_segment.vhd
@@ -3,25 +3,20 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
 entity bcd_to_7_segment is
-port(
-
-	bcd : in std_logic_vector(3 downto 0);
-
-	seven_seg : out std_logic_vector(6 downto 0) := (others => '0')
-
+	port(
+		bcd       : in  std_logic_vector(3 downto 0);
+		dot       : in  std_logic := '0';
+		seven_seg : out std_logic_vector(7 downto 0) := (others => '0')
 	);
 end entity;
-
 
 architecture rtl of bcd_to_7_segment is
 begin
 
 	process(bcd) is
 	begin
-
-			-- The not on every output is because the 7-segment display is using common anode
+		-- The not on every output is because the 7-segment display is using common anode
 
 		seven_seg(0) <= not (bcd(3) or bcd(1) or (bcd(2) and bcd(0)) or (not bcd(2) and not bcd(0)));
 		seven_seg(1) <= not (not bcd(2) or (not bcd(1) and not bcd(0)) or (bcd(1) and bcd(0)));
@@ -30,6 +25,7 @@ begin
 		seven_seg(4) <= not ((not bcd(2) and not bcd(0)) or (bcd(1) and not bcd(0)));
 		seven_seg(5) <= not (bcd(3) or (not bcd(1) and not bcd(0)) or (bcd(2) and not bcd(1)) or (bcd(2) and not bcd(0)));
 		seven_seg(6) <= not (bcd(3) or (bcd(2) and not bcd(1)) or (not bcd(2) and bcd(1)) or (bcd(1) and not bcd(0)));
+		seven_seg(7) <= dot;
 
 	end process;
 
--- top_level.vhd
+++ top_level.vhd
@@ -8,12 +8,12 @@ entity top is
 		KEY           : in  std_logic_vector(1 downto 0);
 		SW            : in  std_logic_vector(9 downto 0);
 		LEDR          : out std_logic_vector(9 downto 0);
-		HEX0          : out std_logic_vector(6 downto 0);
-		HEX1          : out std_logic_vector(6 downto 0);
-		HEX2          : out std_logic_vector(6 downto 0);
-		HEX3          : out std_logic_vector(6 downto 0);
-		HEX4          : out std_logic_vector(6 downto 0);
-		HEX5          : out std_logic_vector(6 downto 0)
+		HEX0          : out std_logic_vector(7 downto 0);
+		HEX1          : out std_logic_vector(7 downto 0);
+		HEX2          : out std_logic_vector(7 downto 0);
+		HEX3          : out std_logic_vector(7 downto 0);
+		HEX4          : out std_logic_vector(7 downto 0);
+		HEX5          : out std_logic_vector(7 downto 0)
 	);
 end entity;
 
@@ -77,36 +77,42 @@ begin
 	inputs_0_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_0_s,
+			dot       => '1',
 			seven_seg => HEX0
 		);
 
 	inputs_1_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_1_s,
+			dot       => '1',
 			seven_seg => HEX1
 		);
 
 	inputs_2_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_0_m,
+			dot       => '0',
 			seven_seg => HEX2
 		);
 
 	inputs_3_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_1_m,
+			dot       => '1',
 			seven_seg => HEX3
 		);
 
 	inputs_4_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_0_h,
+			dot       => '0',
 			seven_seg => HEX4
 		);
 
 	inputs_5_i : entity work.bcd_to_7_segment
 		port map(
 			bcd       => bcd_1_h,
+			dot       => '1',
 			seven_seg => HEX5
 		);
 
